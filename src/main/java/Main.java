a1ce4fcbee9aec5355d314871ef40399550f8acf
Penelope <pennyblack.p@gmail.com>
2020-01-23 16:39:42 +0100

    fixed code alignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{20, 30, 40, 50, 60};
-        System.out.println("array :"+ Arrays.toString(array));
+        System.out.println("array :" + Arrays.toString(array));
 
         reverse(array);
 
@@ -13,15 +13,14 @@ public class Main {
     }
 
 
+    private static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfIndex = array.length / 2;
 
-    private static void reverse(int[] array){
-        int maxIndex = array.length-1;
-        int halfIndex = array.length/2;
-
-        for (int i=0; i< halfIndex; i++){
+        for (int i = 0; i < halfIndex; i++) {
             int temp = array[i];
-            array[i]=array[maxIndex-i];
-            array [maxIndex-i] = temp;
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
 
         }
 
