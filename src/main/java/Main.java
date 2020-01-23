f9f8881d29704f311d935358d2c4fe90766264f2
Penelope <pennyblack.p@gmail.com>
2020-01-23 16:26:01 +0100

    reverse method added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,31 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] array = new int[]{20, 30, 40, 50, 60};
+        System.out.println("array :"+ Arrays.toString(array));
+
+        reverse(array);
+
+        System.out.println("reversed array:" + Arrays.toString(array));
 
     }
 
-    public void reverse(int array){
+
+
+    private static void reverse(int[] array){
+        int maxIndex = array.length-1;
+        int halfIndex = array.length/2;
+
+        for (int i=0; i< halfIndex; i++){
+            int temp = array[i];
+            array[i]=array[maxIndex-i];
+            array [maxIndex-i] = temp;
+
+        }
 
 
     }
+
 }
